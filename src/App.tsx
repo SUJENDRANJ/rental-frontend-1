7988e127689f4e8ac4d541bf40b7df2950fc5e29
sujendranluvmurffin <sujendranluvmurffin@gmail.com>
2025-09-27 13:39:42 +0530

    new

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { ThemeProvider } from './components/providers/ThemeProvider';
+import { Navbar } from './components/layout/Navbar';
+import { Footer } from './components/layout/Footer';
+import { Home } from './pages/Home';
+import { Products } from './pages/Products';
+import { ProductDetail } from './pages/ProductDetail';
+import { Favorites } from './pages/Favorites';
+import { HostDashboard } from './pages/HostDashboard';
+import { CreateProduct } from './pages/CreateProduct';
+import { Toaster } from '@/components/ui/toaster';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ThemeProvider>
+        <Router>
+          <div className="min-h-screen bg-background">
+            <Navbar />
+            
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/favorites" element={<Favorites />} />
+                <Route path="/host/dashboard" element={<HostDashboard />} />
+                <Route path="/host/create-product" element={<CreateProduct />} />
+              </Routes>
+            </main>
+
+            <Footer />
+            <Toaster />
+          </div>
+        </Router>
+      </ThemeProvider>
+    </Provider>
+  );
+}
+
+export default App;
